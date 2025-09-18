ff6d21a01290ecf43145b2196d4e243e3165178c
victorreyesc <victor07reyescarpin@gmail.com>
2025-09-18 14:06:43 -0300

    Adição de comentários
    Comentários adicionados na primeira parte do código.

--- src/menuPrincipal.java
+++ src/menuPrincipal.java
@@ -4,10 +4,10 @@ public class menuPrincipal {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-        char[][] salaDeCinema = new char[10][20];
+        char[][] salaDeCinema = new char[10][20]; // Cria a matriz com os assentos
 
         
-        for (int i = 0; i < salaDeCinema.length; i++) {
+        for (int i = 0; i < salaDeCinema.length; i++) { // Todos assentos são declarados inicialmente como livres
             for (int j = 0; j < salaDeCinema[i].length; j++) {
                 salaDeCinema[i][j] = 'L';
             }
@@ -27,16 +27,18 @@ public class menuPrincipal {
 
             switch (opcao) {
             case 1:
-                   System.out.print("           "); 
-            for (int j = 0; j < salaDeCinema[0].length; j++) {
+                   System.out.print("           "); // Espaço em branco para alinhar as colunas na saída
+                   
+            for (int j = 0; j < salaDeCinema[0].length; j++) { // Percorre todas as colunas da primeira fileira e retorna o seu número
                    System.out.printf("%3d", j + 1); 
             }
             System.out.println();
 
-            for (int i = 0; i < salaDeCinema.length; i++) {
+            for (int i = 0; i < salaDeCinema.length; i++) { // Percorre todas as fileiras e retorna o número de fileiras
                    System.out.printf("Fileira %2d: ", i + 1); 
-            for (int j = 0; j < salaDeCinema[i].length; j++) {
-                   System.out.print("[" + salaDeCinema[i][j] + "]");
+                   
+            for (int j = 0; j < salaDeCinema[i].length; j++) { 
+                   System.out.print("[" + salaDeCinema[i][j] + "]"); // Imprime os assentos
             }
                    System.out.println();
             }
@@ -44,7 +46,9 @@ public class menuPrincipal {
 
                     
             case 2:
- 
+                System.out.println("Selecione o assento desejado para compra:");
+                
+                
                 break;
             
             case 3:
